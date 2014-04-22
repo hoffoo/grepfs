d364bbb353951dd20cdc2c3fc681ffcc11e6acd6
Marin <marin@stayke.net>
2014-04-22 01:00:43 -0700

    fix #3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 
 all:
-	gcc -Wall grepfs_fuse.c grepfs.c `pkg-config fuse --cflags --libs` -o grepfs
+	gcc -Wall -g grepfs_fuse.c grepfs.c `pkg-config fuse --cflags --libs` -o grepfs
 
--- grepfs.c
+++ grepfs.c
@@ -1,7 +1,11 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "grepfs.h"
+
+#define CWD_BUF_SIZE 1024
 
 // real path of "mounted" file
 const char *help = "missing arguments\n";
@@ -23,6 +27,22 @@ int main(int argc, char *argv[])
 
     argv[argc-2] = argv[argc-1];
     argc--;
+
+    // not an absolute path
+    if (file[0] != '/') {
+        char* fbuf = malloc(CWD_BUF_SIZE);
+        char* path = getcwd(fbuf, CWD_BUF_SIZE);
+        if (path == NULL) {
+            return 1;
+        }
+
+        // alloc path + file + /
+        path = realloc(path, strlen(file) + strlen(path) + 1);
+        path = strcat(path, "/");
+        file = strcat(path, file);
+    }
+
+    printf("mounting %s at %s\n", file, mountpoint);
 
     return grepfs_main(argc, argv, file);
 }
